abaeef467510978d3d4738d222a6e13ef00fe284
Padma Gundapaneni <padmag@uw.edu>
2021-05-10 19:49:51 -0700

    push padma's progress

--- eda/exploratory_analysis.R
+++ eda/exploratory_analysis.R
@@ -1,14 +1,27 @@
-surgical_procedures <- read.csv('data/HEALTH_PROC_11052021025404519.csv')
+library(ggplot2)
+
+# Import Data
 hospital_length_of_stay <- read.csv('data/HEALTH_PROC_11052021025328109.csv')
 
+##################################################################################
+# Padma
+# Graph 1: Distribution of the mean of average hospital stay length by country
 agg = aggregate(hospital_length_of_stay,
-                by = list(hospital_length_of_stay$Country),
-                FUN = mean)
+      by = list(hospital_length_of_stay$Country),
+      FUN = median)
 
-# Padma - 1. Distribution of the mean of average hospital stay length by country
-# Ayush - 2. Distribution of the mean of average hospital stay length by year
+colnames(agg) <- c("CountryName", "", "", "", "", "", "", "", "", "Value")
 
-# Dependent variable: average length of stay in hospital
+barplot(table(agg$Value))
 
-# Thomas - 3. make a scatter plot for a single country over time
-# Zikai - 4. make a line plot for all countries in a single year
+##################################################################################
+# Ayush
+# Graph 2: Distribution of the mean of average hospital stay length by year
+
+# Dependent variable = average length of stay in hospital
+
+# Thomas
+# Graph 3: Make a scatter plot for a single country over time
+
+# Zikai
+# Graph 4: Make a line plot for all countries in a single year
