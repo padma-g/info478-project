8d112f99b005c65a1a56da9c62a1b3ce1aedad4d
Padma Gundapaneni <padmag@uw.edu>
2021-05-14 07:04:47 -0700

    Fix plots

--- eda/exploratory_analysis.R
+++ eda/exploratory_analysis.R
@@ -5,29 +5,28 @@ library(dplyr)
 hospital_length_of_stay <- read.csv("data/HEALTH_PROC_11052021025328109.csv")
 
 # Padma
-# Graph 1: Distribution of the mean of average hospital stay length by country
-aggregated_df <- aggregate(hospital_length_of_stay,
-  by = list(hospital_length_of_stay$Country),
-  FUN = mean
-)
+# Graph 1: Distribution of average hospital stay length variable
+filtered_df <- hospital_length_of_stay %>%
+  filter(Variable == "All causes")
 
-colnames(aggregated_df) <- c(
-  "CountryName", "", "", "", "", "", "", "", "",
-  "Value"
-)
-
-plot_hist <- ggplot(aggregated_df, aes(x = Value)) +
-  geom_histogram(color = "grey", binwidth = 1) +
+hist_length_of_stay <- ggplot(data = filtered_df) +
+  geom_histogram(
+    stat = "count", mapping = aes(x = Value), fill = "lightblue",
+    color = "black", binwidth = 30
+  ) +
   labs(
-    x = "Average Hospital Stay (in days)",
-    y = "Number of Countries",
-    title = "Average Stay Length Per Country"
+    title = "Average Length of Stay Frequency Distribution",
+    x = "Average Length of Stay (in days)",
+    y = "Frequency"
   )
 
 # Ayush
-# Graph 2: Distribution of the mean of average hospital stay length by year
+# Graph 2: Distribution of the year variable
 year_distribution_plot <- ggplot(data = hospital_length_of_stay) +
-  geom_histogram(mapping = aes(x = Year), fill = "sea green", bins = 10) +
+  geom_histogram(
+    mapping = aes(x = Year), fill = "lightblue",
+    color = "black", bins = 10
+  ) +
   labs(
     title = "Year Frequency Distribution",
     x = "Year",
@@ -35,7 +34,7 @@ year_distribution_plot <- ggplot(data = hospital_length_of_stay) +
   )
 
 # Thomas
-# Graph 3: Make a scatter plot for a single country over time
+# Graph 3: Make a plot for a single country over time
 new_data <- hospital_length_of_stay %>%
   filter(Variable == "All causes" & Country == "Korea")
 
@@ -48,7 +47,7 @@ korea_plot <- ggplot(data = new_data) +
   )
 
 # Zikai
-# Graph 4: Make a line plot for all countries in a single year
+# Graph 4: Make a plot for all countries in a single year
 barpdata <- hospital_length_of_stay %>%
   filter(Variable == "All causes" & Year == 2000)
 
