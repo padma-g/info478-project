0c60fe283ce2f377386549202865f678bef2adff
Padma Gundapaneni <padmag@uw.edu>
2021-05-13 19:26:58 -0700

    Add relationships between variables graph #2 (Zikai) and lint/style code

--- eda/exploratory_analysis.R
+++ eda/exploratory_analysis.R
@@ -2,28 +2,30 @@ library(ggplot2)
 library(dplyr)
 
 # Import Data
-hospital_length_of_stay <- read.csv('data/HEALTH_PROC_11052021025328109.csv')
+hospital_length_of_stay <- read.csv("data/HEALTH_PROC_11052021025328109.csv")
 
-##################################################################################
 # Padma
 # Graph 1: Distribution of the mean of average hospital stay length by country
-aggregated_df = aggregate(hospital_length_of_stay,
-      by = list(hospital_length_of_stay$Country),
-      FUN = mean)
+aggregated_df <- aggregate(hospital_length_of_stay,
+  by = list(hospital_length_of_stay$Country),
+  FUN = mean
+)
 
-colnames(aggregated_df) <- c("CountryName", "", "", "", "", "", "", "", "", "Value")
+colnames(aggregated_df) <- c(
+  "CountryName", "", "", "", "", "", "", "", "",
+  "Value"
+)
 
-plot_hist<-ggplot(aggregated_df, aes(x=Value)) + 
-  geom_histogram(color="black", fill="lightblue", binwidth=1) +
-  labs(x="Average Hospital Stay (in days)",
-       y="Number of Countries",
-       title="Average Hospital Stay Per Country")
+plot_hist <- ggplot(aggregated_df, aes(x = Value)) +
+  geom_histogram(color = "grey", binwidth = 1) +
+  labs(
+    x = "Average Hospital Stay (in days)",
+    y = "Number of Countries",
+    title = "Average Stay Length Per Country"
+  )
 
-##################################################################################
 # Ayush
 # Graph 2: Distribution of the mean of average hospital stay length by year
-
-# Dependent variable = average length of stay in hospital
 
 # Thomas
 # Graph 3: Make a scatter plot for a single country over time
@@ -35,7 +37,18 @@ korea_plot <- ggplot(data = new_data) +
   labs(
     title = "Average Length of Stay in Hospital (Korea, 2002 - 2018)",
     x = "Years",
-    y = "Average Length of Stay")
+    y = "Average Length of Stay"
+  )
 
 # Zikai
 # Graph 4: Make a line plot for all countries in a single year
+barpdata <- hospital_length_of_stay %>%
+  filter(Variable == "All causes" & Year == 2000)
+
+barp <- ggplot(data = barpdata) +
+  geom_col(mapping = aes(x = reorder(Country, Value), y = Value)) +
+  coord_flip() +
+  labs(
+    title = "Average Length of Stay in Hospital in 2000 by Country",
+    x = "Country", y = "Average Length of Stay in Hospital (in days)"
+  )
