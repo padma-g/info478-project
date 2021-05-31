d5fec58b2fd7616a7b9a024e7dd2cb36ac71c4e9
Padma Gundapaneni <padmag@uw.edu>
2021-05-31 10:01:52 -0700

    lint and style

--- server.R
+++ server.R
@@ -9,43 +9,47 @@ data <- read.csv("eda/data/HEALTH_PROC_11052021025328109.csv")
 data(world.cities)
 
 server <- function(input, output) {
-    output$plot_data <- renderPlot({
-        plot_data1 <- data %>%
-            filter(Year >= 2000 & Year <= 2018) %>%
-            ungroup() %>%
-            group_by(Year, Country) %>%
-            summarise(Stay = mean(Value)) %>% 
-            ungroup() %>%
-            filter(Country==input$Country)
-        
-        lh_plot <- ggplot(plot_data1) +
-            geom_line(aes(x = Year,
-                          y = Stay), group = 1) +
-            labs(title = "Hospital Average Length of Stay by Countries (2000 - 2018)",
-                 x = "Year", y = paste0("Average Length of Stay for ", input$Country),
-                 color = "Country")
-        
-        lh_plot
-    })
-    
-    output$map <- renderLeaflet({
-        df <- world.cities %>%
-            filter(capital == 1) %>%
-            dplyr::select(Country = country.etc, lat, lng = long) %>%
-            left_join(hospital_length_of_stay, ., by = "Country") %>% 
-            filter(Year==input$Year)
-        
-        map <- leaflet(df) %>%
-            addProviderTiles("CartoDB.Positron") %>%
-            addCircleMarkers(
-                lat = ~lat,
-                lng = ~lng,
-                radius = ~ Value / 5,
-                color = "red",
-                stroke = FALSE,
-                popup = paste0("Country: ", df$Country)
-            )
-        
-        map
-    })
+  output$plot_data <- renderPlot({
+    plot_data1 <- data %>%
+      filter(Year >= 2000 & Year <= 2018) %>%
+      ungroup() %>%
+      group_by(Year, Country) %>%
+      summarise(Stay = mean(Value)) %>%
+      ungroup() %>%
+      filter(Country == input$Country)
+
+    lh_plot <- ggplot(plot_data1) +
+      geom_line(aes(
+        x = Year,
+        y = Stay
+      ), group = 1) +
+      labs(
+        title = "Hospital Average Length of Stay by Countries (2000 - 2018)",
+        x = "Year", y = paste0("Average Length of Stay for ", input$Country),
+        color = "Country"
+      )
+
+    lh_plot
+  })
+
+  output$map <- renderLeaflet({
+    df <- world.cities %>%
+      filter(capital == 1) %>%
+      dplyr::select(Country = country.etc, lat, lng = long) %>%
+      left_join(hospital_length_of_stay, ., by = "Country") %>%
+      filter(Year == input$Year)
+
+    map <- leaflet(df) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addCircleMarkers(
+        lat = ~lat,
+        lng = ~lng,
+        radius = ~ Value / 5,
+        color = "red",
+        stroke = FALSE,
+        popup = paste0("Country: ", df$Country)
+      )
+
+    map
+  })
 }
--- ui.R
+++ ui.R
@@ -7,64 +7,89 @@ library(DT)
 data <- read.csv("eda/data/HEALTH_PROC_11052021025328109.csv")
 
 intro_panel <- tabPanel(
-    "Introduction",
-    p("Group: Padma Gundapaneni, Ayush Banwari, Thomas Lee, Zikai Song"),
-    h4("Purpose"),
-    p("In the recent global pandemic of COVID-19, we noticed that a great number of countries faced or are facing the issue of medical resource shortage. People lost their lives because of the lack of beds, ICU spots, or even oxygen. This phenomenon raised our concerns in medical and healthcare resource and utilization data. In this research, examined the healthcare usage data by countries to uncover some insights about the distribution and utilization of healthcare resources across the globe."),
-    h4("Data"),
-    p("The dataset we used comes from the Organisation for Economic Co-operation (OECD). The dataset contains healthcare utilization data from many different countries from 2000 to 2018. It includes data about the average length of stay in a hospital by year and country, as well as by medical condition. The data was collected by OECD from a variety of credible health departments across the world. The data was accessed from the OECD website, at the following", tags$a(href = "https://stats.oecd.org/Index.aspx?DataSetCode=HEALTH_PROC#", "link"), ".")
+  "Introduction",
+  p("Group: Padma Gundapaneni, Ayush Banwari, Thomas Lee, Zikai Song"),
+  h4("Purpose"),
+  p("In the recent global pandemic of COVID-19, we noticed that a great
+    number of countries faced or are facing the issue of medical resource
+    shortage. People lost their lives because of the lack of beds,
+    ICU spots, or even oxygen. This phenomenon raised our concerns in
+    medical and healthcare resource and utilization data. In this research,
+    examined the healthcare usage data by countries to uncover some insights
+    about the distribution and utilization of healthcare resources across
+    the globe."),
+  h4("Data"),
+  p("The dataset we used comes from the Organisation for Economic Co-operation
+    (OECD). The dataset contains healthcare utilization data from many different
+    countries from 2000 to 2018. It includes data about the average length of
+    stay in a hospital by year and country, as well as by medical condition.
+    The data was collected by OECD from a variety of credible health
+    departments across the world. The data was accessed from the OECD website,
+    at the following",
+    tags$a(href = "https://stats.oecd.org/Index.aspx?DataSetCode=HEALTH_PROC#",
+           "link"), ".")
 )
 
 year_panel <- tabPanel(
-    "Hospital Average Length of Stay by Countries",
-        sidebarLayout(
-            sidebarPanel(
-                h4("Adjustable Parameters"),
-                selectInput(
-                    inputId = "Country",
-                    label = "Country",
-                    choices = c("Australia", "Austria", "Belgium", "Canada", "Chile", "Costa Rica", "Czech Republic", "Denmark", "Estonia", "Finland", "France", 
-                                "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Israel", "Italy", "Korea", "Latvia", "Lithuania", "Luxembourg", "Mexico", 
-                                "Netherlands", "New Zealand", "Norway", "Poland", "Portugal", "Slovak Republic", "Slovenia", "Spain", "Sweden", "Switzerland", 
-                                "Turkey", "United Kingdom", "United States"),
-                    selected = "Australia"
-                )),
-            mainPanel(
-                h4("Scatterplot"),
-                plotOutput(outputId = "plot_data"),
-            )
-        )
+  "Hospital Average Length of Stay by Countries",
+  sidebarLayout(
+    sidebarPanel(
+      h4("Adjustable Parameters"),
+      selectInput(
+        inputId = "Country",
+        label = "Country",
+        choices = c(
+          "Australia", "Austria", "Belgium", "Canada", "Chile", "Costa Rica",
+          "Czech Republic", "Denmark", "Estonia", "Finland", "France",
+          "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Israel",
+          "Italy", "Korea", "Latvia", "Lithuania", "Luxembourg",
+          "Mexico", "Netherlands", "New Zealand", "Norway", "Poland",
+          "Portugal", "Slovak Republic", "Slovenia", "Spain", "Sweden",
+          "Switzerland", "Turkey", "United Kingdom", "United States"
+        ),
+        selected = "Australia"
+      )
+    ),
+    mainPanel(
+      h4("Scatterplot"),
+      plotOutput(outputId = "plot_data"),
+    )
+  )
 )
 
 map_panel <- tabPanel(
-    "Hospital Average Length of Stay by Year",
-    sidebarLayout(
-        sidebarPanel(
-            h4("Adjustable Parameters"),
-            selectInput(
-                inputId = "Year",
-                label = "Year",
-                choices = c("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"),
-                selected = "2000"
-            )),
-        mainPanel(
-            h4("Map"),
-            leafletOutput(outputId = "map"),
-        )
+  "Hospital Average Length of Stay by Year",
+  sidebarLayout(
+    sidebarPanel(
+      h4("Adjustable Parameters"),
+      selectInput(
+        inputId = "Year",
+        label = "Year",
+        choices = c("2000", "2001", "2002", "2003", "2004", "2005",
+                    "2006", "2007", "2008", "2009", "2010", "2011",
+                    "2012", "2013", "2014", "2015", "2016", "2017",
+                    "2018"),
+        selected = "2000"
+      )
+    ),
+    mainPanel(
+      h4("Map"),
+      leafletOutput(outputId = "map"),
     )
+  )
 )
 
 insights_panel <- tabPanel(
-    "Insights and Conclusion"
+  "Insights and Conclusion"
 )
 
 ui <- fluidPage(
-    includeCSS("style.css"),
-    navbarPage(
-        "Healthcare Utilization Analysis",
-        intro_panel,
-        year_panel,
-        map_panel,
-        insights_panel
-    )
+  includeCSS("style.css"),
+  navbarPage(
+    "Healthcare Utilization Analysis",
+    intro_panel,
+    year_panel,
+    map_panel,
+    insights_panel
+  )
 )
